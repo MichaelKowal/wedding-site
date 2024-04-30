a57947fb0c319d7553865843355522f833efc914
Michael Kowal <mkowal@latitudegeo.com>
2024-04-30 15:33:09 -0700

    fix ios styling

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -9,14 +9,15 @@ const Start: React.FC = () => {
   }
   return (
     <section id="Start" className="bg-cornsilk">
-      <div className="flex md:flex-row flex-col">
-        <div className="sm:w-1/2 w-full absolute h-100">
+      <div className="flex md:flex-row flex-col" >
+        <div className="bg-cornsilk h-screen md:w-1/2 w-full flex items-center sm:pt-32 pt-12 relative">
+          
+        <div className="sm:w-1/2 w-full absolute top-0 left-0">
         <img src={flowerTop} alt="border" className="sm:pt-20 p-0"/>
         </div>
-        <div className="sm:w-1/2 w-full absolute bottom-0">
+        <div className="sm:w-1/2 w-full absolute bottom-0 right-0">
         <img src={flowerBottom} alt="border"/>
         </div>
-        <div className="bg-cornsilk h-screen md:w-1/2 w-full flex items-center sm:pt-32 pt-12">
           <div className="border-4 border-cornsilk-200 m-8 mb-20 flex flex-col justify-center items-center h-full w-full sm:px-20 px-8">
             <div className="text-center text-dark_moss_green font-alex-brush sm:text-6xl text-5xl">
               Michael and Georgia's Wedding
