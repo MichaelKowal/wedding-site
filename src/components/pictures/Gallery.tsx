006bf1f318dbdbf1ebef9700f7b590721d1c36f3
Michael Kowal <mkowal@latitudegeo.com>
2024-08-05 19:27:47 -0700

    add counter and buttons

--- src/components/pictures/Gallery.tsx
+++ src/components/pictures/Gallery.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
@@ -35,6 +35,8 @@ const PhotoGallery: React.FC = () => {
   const [images, setImages] = useState<ImageDesc[]>([]);
   const [fullScreen, setFullScreen] = useState<boolean>(false);
   const [fsImage, setFSImage] = useState<string>("");
+  const [index, setIndex] = useState<number>(1);
+  const carousel = useRef<Carousel | null>();
 
   const loadImages = () => {
     const urlArray: string[] = [];
@@ -61,15 +63,44 @@ const PhotoGallery: React.FC = () => {
     setFSImage("");
   };
 
+  const handleMovePictureForward = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (carousel.current) {
+      const nextSlide = carousel.current.state.currentSlide + 1;
+
+      if (nextSlide < images.length) {
+        setFSImage(images[nextSlide].src);
+        carousel.current.goToSlide(nextSlide);
+      }
+    }
+    e.stopPropagation();
+  };
+
+  const handleMovePictureBackward = (
+    e: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    if (carousel.current) {
+      const prevSlide = carousel.current.state.currentSlide - 1;
+
+      if (prevSlide) {
+        setFSImage(images[prevSlide].src);
+        carousel.current.goToSlide(prevSlide);
+      }
+    }
+    e.stopPropagation();
+  };
+
   return (
-    <div className="bg-cornsilk h-screen">
+    <div className="bg-cornsilk h-screen overflow-hidden">
       <div className="text-center text-dark_moss_green font-alex-brush sm:text-6xl text-5xl pt-4 absolute top-0 w-full">
         Michael and Georgia's Wedding
       </div>
       <Carousel
         responsive={responsive}
         centerMode={true}
-        containerClass="h-full"
+        ref={(el) => (carousel.current = el)}
+        afterChange={(_prev, { currentSlide }) => {
+          setIndex(currentSlide + 1);
+        }}
       >
         {images.map((image) => (
           <div
@@ -86,13 +117,32 @@ const PhotoGallery: React.FC = () => {
           style={{ zIndex: 1200 }}
           onClick={handleCloseFullscreen}
         >
+          {index > 1 && (
+            <button
+              className="rounded-full w-20 h-20 text-3xl p-3 bg-dark_moss_green-200 hover:bg-dark_moss_green-300 bg-opacity-75 text-cornsilk-400 absolute left-0 m-5"
+              onClick={handleMovePictureBackward}
+            >
+              {"<"}
+            </button>
+          )}
           <img
             src={fsImage}
             style={{ maxHeight: "90%" }}
             onClick={(event) => event.stopPropagation()}
           />
+          {index < images.length && (
+            <button
+              className="rounded-full w-20 h-20 text-3xl p-3 bg-dark_moss_green-200 hover:bg-dark_moss_green-300 bg-opacity-75 text-cornsilk-400 absolute right-0 m-5"
+              onClick={handleMovePictureForward}
+            >
+              {">"}
+            </button>
+          )}
         </div>
       )}
+      <div className="w-full absolute bottom-0 text-center text-dark_moss_green-500 mb-10 text-xl">
+        {index}/{images.length}
+      </div>
     </div>
   );
 };
